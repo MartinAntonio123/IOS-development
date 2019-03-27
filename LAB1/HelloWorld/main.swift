b43510e12b2c8bbac475ba3ed783d64239da8f2d
Martin Vivanco <martinvivanco@Martins-iMac.local>
2019-03-27 16:38:56 -0700

    starting lab1

---
+++ LAB1/HelloWorld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by Martin Vivanco on 27/3/19.
+//  Copyright © 2019 Martin Vivanco. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
