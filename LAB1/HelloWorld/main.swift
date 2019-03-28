ebf7d0926979b962083f01b907920c8fdc930753
Martin Vivanco <martinvivanco@Martins-iMac.local>
2019-03-27 18:02:28 -0700

    verify if number is a prime basic algorithm

--- LAB1/HelloWorld/main.swift
+++ LAB1/HelloWorld/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Please enter a positive integer: ")
+let input: Int? = Int(readLine()!)
+if input == nil {
+    print("Input was not a number")
+    exit(1)
+}
 
+let num: Int = input!
+if num < 1 {
+    print("Number was not positive")
+    exit(2)
+}
+
+var isPrime: Bool = true
+let max: Double = Double(num).squareRoot()
+for i in 2...Int(max) {
+    if num % i == 0 {
+        isPrime = false
+        break
+    }
+}
+
+if (isPrime) {
+    print("\(num) is prime")
+}
+else {
+    print("\(num) is not prime")
+}
